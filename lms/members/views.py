4a6392e13aee06d4a27ea43b4757ff7fce5f0e92
NextGen-Techno-iq <mhmn0201@gmail.com>
2024-01-14 18:51:18 +0300

    First commit

---
+++ lms/members/views.py
@@ -0,0 +1,37 @@
+# views.py
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Student
+from .serializer import StudentSerializer
+from django.contrib.auth.hashers import make_password, check_password
+
+
+class SignUpView(APIView):
+    def post(self, request):
+        serializer = StudentSerializer(data=request.data, context={'request':request})
+        if serializer.is_valid():
+            # Hash the password before saving it to the database
+            serializer.validated_data['password'] = make_password(serializer.validated_data['password'])
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        phone = request.data.get('phone', '')
+        password = request.data.get('password', '')
+        try:
+            student = Student.objects.get(phone=phone)
+            if check_password(password, student.password):
+                serializer = StudentSerializer(student, context={'request': request})
+                user_data = serializer.data
+                return Response({'message': 'Login successful!', 'user': user_data}, status=status.HTTP_200_OK)
+            else:
+                return Response({'message': 'Invalid password'}, status=status.HTTP_401_UNAUTHORIZED)
+        except ObjectDoesNotExist:
+            return Response({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
